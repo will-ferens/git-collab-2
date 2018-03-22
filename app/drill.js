58ece39944628ad3a7acb17b0b287197cabeeee3
David Aspesi <davidaspesi@Davids-MacBook-Pro-3.local>
2018-03-22 11:52:39 -0600

    created an array of objects

---
+++ app/drill.js
@@ -0,0 +1,13 @@
+instruments = [{
+    name: "Fender Jazz",
+    age: 12
+},{
+    name: "Fender Blacktop",
+    age: 4
+},{
+    name: "Jackson Mini-V",
+    age: 3
+},{
+    name: "Fender Jag",
+    age: 2
+}]
